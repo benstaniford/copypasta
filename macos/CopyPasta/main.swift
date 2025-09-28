d798ac454e0866366f7ee8e1526eeebd55604fac
Ben Staniford <bstaniford@beyondtrust.com>
2025-09-28 10:05:36 +0100

    remove unused entitlement and simplify main.swift execution logic

--- macos/CopyPasta/main.swift
+++ macos/CopyPasta/main.swift
@@ -8,29 +8,10 @@ try? debugMessage.write(toFile: testFilePath, atomically: true, encoding: .utf8)
 
 NSLog("=== CopyPasta main.swift STARTED ===")
 print("=== CopyPasta main.swift STARTED ===")
-fflush(stdout)
 
-// Show an alert dialog to prove our code is running
-DispatchQueue.main.async {
-    let alert = NSAlert()
-    alert.messageText = "CopyPasta Debug"
-    alert.informativeText = "main.swift is executing!"
-    alert.runModal()
-}
+let app = NSApplication.shared
+let delegate = AppDelegate()
+app.delegate = delegate
 
-do {
-    NSLog("Creating NSApplication.shared")
-    let app = NSApplication.shared
-    
-    NSLog("Creating AppDelegate")
-    let delegate = AppDelegate()
-    
-    NSLog("Setting app delegate")
-    app.delegate = delegate
-    
-    NSLog("About to call app.run()")
-    app.run()
-} catch {
-    NSLog("ERROR in main.swift: \(error)")
-    print("ERROR in main.swift: \(error)")
-}
+NSLog("About to call app.run()")
+app.run()
